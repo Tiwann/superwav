996fbf1ad0da1284ea63537d7555dd14791b856e
Tiwann <tiwannbeats@gmail.com>
2022-04-02 15:27:37 +0200

    Update Premake scripts

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,20 @@
+require "cleanup"
+
 workspace "superwav"
     architecture "x86_64"
     startproject "superwav"
     configurations { "Debug", "Release" }
     flags "MultiProcessorCompile"
 
-bindir = "binaries"
-intdir = "binaries-int"
-
-newaction 
-{
-    trigger = "clean",
-    description = "Clean all the generated files",
-    
-    onStart = function()
-        print("Cleaning all the generated files...")
-    end,
-
-    execute = function()
-        os.rmdir("binaries")
-        os.rmdir("binaries-int")
-        os.rmdir(".vs")
-        os.rmdir(".idea")
-        os.remove("**.vcxproj")
-        os.remove("**.vcxproj.filters")
-        os.remove("**.vcxproj.user")
-        os.remove("**.sln")
-        os.remove("**.make")
-        os.remove("**Makefile")
-    end,
-
-    onEnd = function()
-        print("Done.")
-    end
-}
+bindir = "%{wks.location}/binaries"
+intdir = "%{wks.location}/binaries-obj"
 
 project "superwav"
     location "superwav"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++20"
-    systemversion "latest"
+    systemversion "Latest"
 
     targetdir(bindir)
     objdir(intdir)
@@ -67,38 +42,10 @@ project "superwav"
 
     filter "configurations:Debug"
         runtime "Debug"
-        symbols "on"
-        defines "DEBUG"
+        symbols "On"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "on"
+        optimize "On"
 
-project "wavlib"
-    location "wavlib"
-    kind "sharedlib"
-    language "C++"
-    cppdialect "C++17"
-    staticruntime "off"
-
-    targetdir(bindir)
-    objdir(intdir)
-
-    files
-    {
-        "%{prj.location}/include/*.h",
-        "%{prj.location}/source/*.cpp",
-    }
-
-    includedirs
-    {
-        "%{prj.location}/include"
-    }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        symbols "on"
-
-    filter "configurations:Release"
-        runtime "Release"
-        optimize "on"
+include "wavlib"
