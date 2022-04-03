b465e5693bcb0700765b697ab76c06211e4d92f6
Tiwann <tiwannbeats@gmail.com>
2022-04-03 21:46:16 +0200

    Misc changes

--- superwav/source/main.cpp
+++ superwav/source/main.cpp
@@ -1,23 +1,21 @@
 #include <wavlib.h>
 #include <generator.h>
 
-
-
 int main(const int argc, const char** argv)
 {
-    wavlib::wav audio;
+    wavlib::wav audio(R"(C:\Users\Tiwann\Desktop\Start Your Engines.wav)");
 
     wavlib::generators::sine sine(1.0f, 440, 0.5f);
     audio = sine.towav();
     audio.write_to_file("sine.wav");
 
-    wavlib::generators::square square(1.0f, 440, 0.5f);
-    audio = square.towav();
-    audio.write_to_file("square.wav");
+    //wavlib::generators::square square(1.0f, 440, 0.5f);
+    //audio = square.towav();
+    //audio.write_to_file("square.wav");
 
-    wavlib::generators::triangle tri(1.0f, 440, 0.5f); 
-    audio = tri.towav();
-    audio.write_to_file("triangle.wav");
+    //wavlib::generators::triangle tri(1.0f, 440, 0.5f); 
+    //audio = tri.towav();
+    //audio.write_to_file("triangle.wav");
 
     return 0;
 }
